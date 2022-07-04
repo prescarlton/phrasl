5e35bf6b9628411ece922f5f9559251c605c1363
Preston <prescarlton@gmail.com>
2022-07-04 18:36:35 -0400

    added 13k puzzles

--- src/puzzles/index.tsx
+++ src/puzzles/index.tsx
@@ -1,5 +1,41 @@
-import phrase from "./categories/phrase"
 import beforeAndAfter from "./categories/beforeAndAfter"
+import event from "./categories/event"
+import foodAndDrink from "./categories/foodAndDrink"
+import funAndGames from "./categories/funAndGames"
+import landmark from "./categories/landmark"
+import livingThing from "./categories/livingThing"
+import livingThings from "./categories/livingThings"
+import occupation from "./categories/occupation"
 import onTheMap from "./categories/onTheMap"
+import people from "./categories/people"
+import person from "./categories/person"
+import phrase from "./categories/phrase"
+import places from "./categories/places"
+import rhymeTime from "./categories/rhymeTime"
+import sameName from "./categories/sameName"
+import showbiz from "./categories/showbiz"
+import thing from "./categories/thing"
+import things from "./categories/things"
+import whatAreYouDoing from "./categories/whatAreYouDoing"
 
-export default [...phrase, ...beforeAndAfter, ...onTheMap]
+export default [
+  ...beforeAndAfter,
+  ...event,
+  ...foodAndDrink,
+  ...funAndGames,
+  ...landmark,
+  ...livingThing,
+  ...livingThings,
+  ...occupation,
+  ...onTheMap,
+  ...people,
+  ...person,
+  ...phrase,
+  ...places,
+  ...rhymeTime,
+  ...sameName,
+  ...showbiz,
+  ...thing,
+  ...things,
+  ...whatAreYouDoing,
+]
